1f014f2f80c5da0eebd731dae0ff51f149601c65
Your Name <you@example.com>
2017-01-30 02:45:24 +0000

    ss

--- chef-repo/cookbooks/sample/recipes/default.rb
+++ chef-repo/cookbooks/sample/recipes/default.rb
@@ -7,18 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
-force_default['form'] = 'sample'
-log 'Hello World'
+#force_default['form'] = 'sample'
+#log 'Hello World'
+#
+#log "Server Name= #{node['serverx']}"
 
-log "Server Name= #{node['serverx']}"
+#node['servery'].each do |rund|
+#  log "Server Y Name= #{rund}"
+#end
+#
+#log "Other contnt #{node['sample']}"
+#
+#log "package name = #{node['pack_name']}"
+#
+#prime_myfile 
+##log "form = #{node['form']}"
 
-node['servery'].each do |rund|
-  log "Server Y Name= #{rund}"
-end
-
-log "Other contnt #{node['sample']}"
-
-log "package name = #{node['pack_name']}"
-
-prime_myfile 
-log "form = #{node['form']}"
+log "#{node['packages']['centos-release']['version']}"
